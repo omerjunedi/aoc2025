c2356df866ec2720dd50bf2b14e7edda173897b8
omerjunedi <ojunedi@umich.edu>
2025-12-31 20:48:55 -0500

    part 2

--- day7/solution.py
+++ day7/solution.py
@@ -1,3 +1,4 @@
+from functools import cache
 import sys
 
 D = sys.stdin.read().splitlines()
@@ -7,7 +8,7 @@ N = len(D)
 M = len(D[0])
 
 
-def recurse(row: int, col: int, visited: set[tuple[int, int]], res: list[int]) -> None:
+def part1(row: int, col: int, visited: set[tuple[int, int]], res: list[int]) -> None:
 
     if col not in range(M):
         return
@@ -20,13 +21,35 @@ def recurse(row: int, col: int, visited: set[tuple[int, int]], res: list[int]) -
             if (r, col) not in visited:
                 res[0] += 1
                 visited.add((r, col))
-                recurse(r+1, col-1, visited, res)
-                recurse(r+1, col+1, visited, res)
+                part1(r+1, col-1, visited, res)
+                part1(r+1, col+1, visited, res)
             return
+
+
+@cache
+def part2(row: int, col: int) -> int:
+
+    # complete path
+    if row == N - 1:
+        return 1
+
+    if col not in range(M):
+        return 0
+
+    if row not in range(N):
+        return 0
+
+    for r in range(row, N):
+        if D[r][col] == '^':
+            return part2(r+1, col-1) + part2(r+1, col+1)
+
+    return 1
+
 
 
 res = [0]
 visited: set[tuple[int, int]] = set()
-
-recurse(0, start, visited, res)
+part1(0, start, visited, res)
 print(res[0])
+
+print(part2(0, start))
